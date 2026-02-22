4f7bec93f81d515e753058368b1fc96c6f4b2116
Jackson202-svg <zwtwwkg65k@privaterelay.appleid.com>
2026-02-23 06:43:02 +0900

    Add car catalog and upgrade parts data

---
+++ src/data.js
@@ -0,0 +1,10 @@
+const carCatalog = [
+    { id: 1, name: "Sedan", price: 5000, value: 7500 },
+    { id: 2, name: "Sports Coupe", price: 15000, value: 22000 },
+    { id: 3, name: "Supercar", price: 50000, value: 85000 }
+];
+
+const upgradeParts = [
+    { name: "Turbo", cost: 1000, multiplier: 1.2 },
+    { name: "Spoilers", cost: 500, multiplier: 1.05 }
+];
